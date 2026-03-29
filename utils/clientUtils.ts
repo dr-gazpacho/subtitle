7a7b74636ee82178593e8c664be2b818a6032596
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2026-03-29 14:31:50 -0500

    handle punctuation

--- utils/clientUtils.ts
+++ utils/clientUtils.ts
@@ -36,18 +36,37 @@ export const genericFetch = async <T>(
   }
 };
 
+/**
+ * maps through speechmatics transcript, simplifies dataset
+ * @param data Speechmatics transcript
+ * @returns simplfied dataset that contains speaker name, timestamps, and word
+ */
 export const getSyncTranscript = (data: SpeechmaticsBatchResponse | null) => {
-  if (!data) {
-    // let the null pass through and don't process - handle null outside the function
-    return data;
-  } else {
-    return data.results
-      .filter((item): item is WordResult => item.type === "word")
-      .map((item) => ({
-        word: item.alternatives[0].content, // TO DO - handle alternatives.length > 1; these edge cases are not included in dataset, solution deferred
+  if (!data) return null;
+
+  const words: WordResult[] = [];
+  const results = data.results;
+
+  for (let i = 0; i < results.length; i++) {
+    const item = results[i];
+
+    if (item.type === "word") {
+      let content = item.alternatives[0].content; // brittle but will work for the dataset- hypothetical do to is nested loop and extract "highest confidence" result
+
+      // need to look ahead to identify if the next item is punctuation and then attach it to the previous word
+      const nextItem = results[i + 1];
+      if (nextItem && nextItem.type === "punctuation") {
+        content += nextItem.alternatives[0].content;
+        i++; // skip ahead if we attach punctionation
+      }
+
+      words.push({
+        word: content,
         start: item.start_time,
         end: item.end_time,
-        speaker: item.alternatives[0].speaker ?? "",
-      }));
+        speaker: item.alternatives[0].speaker ?? "Unknown",
+      });
+    }
   }
+  return words;
 };
